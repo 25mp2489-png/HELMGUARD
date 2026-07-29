e583c9ee7f68c537c1a576d9c17106e1677267de
Anjali <anjalipottayil2003@gmail.com>
2026-07-29 20:19:51 +0530

    Day 7 - Created Flask routes and project structure

---
+++ check.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy
+from ultralytics import YOLO
+from flask import Flask
+import mysql.connector
+
+print("All libraries imported successfully!")
---
+++ test.py
@@ -0,0 +1,1 @@
+print("HELMGUARD Project Started Successfully")
